a213e55ff351b68300b6f4b244ec1e192d1d8a25
unknown <1094610660@qq.com>
2024-05-11 14:35:47 +0800

    正常提交

--- lt/debugtalk.py
+++ lt/debugtalk.py
@@ -10,16 +10,22 @@ gmail_fapth = os.path.dirname(os.path.dirname("getMail.py"))
 sys.path.append(gmail_fapth)
 from getMail import getMail
 import re
+import random
+import string
+
 
 def d_rsa_decrypt(text):
+    """使用私钥进行解密"""
     result = rsa_decrypt(text)
     return result
 
 def d_rsa_encrypt(text):
+    """使用公钥进行加密"""
     result = rsa_encrypt(text)
     return result
 
 def getCode():
+    """获取验证码"""
     pattern = r'\b\d{6}\b'
     mail = getMail()
     verification_code = re.search(pattern, mail)
@@ -29,7 +35,24 @@ def getCode():
         return None
 
 def sleep(n_secs):
+    """睡眠等待"""
     time.sleep(n_secs)
+
+def randomChoice(num):
+    """随机选择"""
+    randomNum = random.randint(0,num-1)
+    return randomNum
+
+def randomLge():
+    """随机选择语言"""
+    language_list = ["zh","en","ja","de"]
+    return random.choice(language_list)
+
+def generate_random_string(num):
+    """生成随机字符"""
+    chars = string.ascii_letters + string.digits
+    return ''.join(random.choice(chars) for _ in range(num))
+
 
 def sum(*args):
     result = 0
@@ -76,6 +99,9 @@ def teardown_hook_example(name):
     logging.warning("teardown_hook_example")
     return f"teardown_hook_example: {name}"
 
+
 if __name__ == '__main__':
-    reslut = getCode()
-    print(reslut)
+    # a = randomChoice(83)
+    # print(a)
+    a = generate_random_string(500)
+    print(a)
